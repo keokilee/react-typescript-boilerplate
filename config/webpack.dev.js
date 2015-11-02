e82462d13b550d29c561663f4ed43f420e67fb82
George Lee <keoki.lee@gmail.com>
2015-11-01 21:41:40 -1000

    Remove unnecessary optimization in dev.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -25,7 +25,6 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
